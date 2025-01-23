c6a848820ceafdc8106ef43f9d9918b7b0be1bf8
morriswa <william@morriswa.org>
2025-01-23 17:02:12 -0600

    # cleaning from fork

--- src/app/views.py
+++ src/app/views.py
@@ -3,7 +3,7 @@ from typing import override
 from django.conf import settings
 from rest_framework.views import APIView
 
-from app.permissions import DJANGO_USER_PERMISSION_CLASSES, DJANGO_MOD_PERMISSION_CLASSES
+from app.permissions import DJANGO_USER_PERMISSION_CLASSES
 from app.authentication import DJANGO_USER_AUTHENTICATION_CLASSES
 
 
@@ -16,10 +16,4 @@ class AnyView(APIView):
 class UserView(APIView):
     """ inherit this class to create a view for unsecured requests """
     permission_classes = DJANGO_USER_PERMISSION_CLASSES
-    authentication_classes = DJANGO_USER_AUTHENTICATION_CLASSES
-
-
-class ModView(APIView):
-    """ inherit this class to create a view for unsecured requests """
-    permission_classes = DJANGO_MOD_PERMISSION_CLASSES
     authentication_classes = DJANGO_USER_AUTHENTICATION_CLASSES
