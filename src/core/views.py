1029542bb86d1c6c866b198bed67a5896f2c543e
Kevin <kevin.rivers14832@ku.edu>
2025-02-02 15:30:37 -0600

    Updated view.py functions.

--- src/core/views.py
+++ src/core/views.py
@@ -5,17 +5,18 @@ from app.decorators import any_view, user_view, unverified_user_view
 
 
 @any_view(['GET'])
-def health(request):
+def health(request: Request):
+    """ health endpoint to test any_view """
     return Response(status=200, data={'msg': 'hello world!'})
 
 @unverified_user_view(['GET'])
-def secure_health(request):
+def secure_health(request: Request):
     return Response(status=200, data={'msg': 'hello secure world!'})
 
 @unverified_user_view(['GET'])
-def permissions(request):
+def permissions(request: Request):
     return Response(status=200, data=request.user.permissions or [])
 
 @user_view(['GET'])
-def permissions(request):
+def permissions(request: Request):
     return Response(status=200, data=request.user.permissions or [])
