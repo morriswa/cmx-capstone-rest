dc25943d70000e7b0446ea02f3c55459a4703666
Kevin <kevin.rivers14832@ku.edu>
2025-02-02 15:27:22 -0600

    Adding to core view.py

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,6 @@
 
 from rest_framework.response import Response
-
+from rest_framework.request import Request
 from app.decorators import any_view, user_view, unverified_user_view
 
 
@@ -13,5 +13,9 @@ def secure_health(request):
     return Response(status=200, data={'msg': 'hello secure world!'})
 
 @unverified_user_view(['GET'])
+def permissions(request):
+    return Response(status=200, data=request.user.permissions or [])
+
+@user_view(['GET'])
 def permissions(request):
     return Response(status=200, data=request.user.permissions or [])
