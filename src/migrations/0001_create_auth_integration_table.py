2e5e6db40b74c586e1ca3aebef99579c9a7e162b
Timothy Holmes <holmestimo11@gmail.com>
2025-03-15 09:44:13 -0500

    comments for core/migrations

--- src/migrations/0001_create_auth_integration_table.py
+++ src/migrations/0001_create_auth_integration_table.py
@@ -1,19 +1,23 @@
 from django.db import migrations
 
-
+'''
+Django migration for auth_integration tables
+'''
 class Migration(migrations.Migration):
     """
         Django migration for auth_integration tables
         :author William Morris """
-    dependencies = []
-    operations = [
+    dependencies = [] #dependencies for the migration
+    operations = [ #List of operations that will be performe during the migration
         migrations.RunSQL(
-            sql="""
+            #SQL statement to creat auth integration table to generat uuid and email
+            sql=""" 
                 create table auth_integration(
                     user_id uuid primary key default gen_random_uuid(),
                     email varchar(256) not null unique
                 );
             """,
+            #SQL statement to drop the auth integration table
             reverse_sql="""
                 drop table if exists auth_integration;
             """
